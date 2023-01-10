6641d289d4869a80cdf3e0b32cd55603f4c7a538
DanielleSmikle <dsmikle97@gmial.com>
2023-01-10 01:43:17 -0500

    testing

--- gifted/admin.py
+++ gifted/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Collection, Feature
 
-# Register your models here.
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Feature)
+class FeatureAdmin(admin.ModelAdmin):
+        list_display =['scent','description','slug','price','in_stock', 'created']
+        list_filter = ['in_stock', 'is_active']
+        list_editable = ['price', 'in_stock']
+        prepopulated_fields = {'slug': ('scent',)}
--- gifted/models.py
+++ gifted/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Collection(models.Model):
+    name= models.CharField(max_length =255, db_index = True)
+    slug = models.SlugField(max_length=255, unique=True, default='collection')
+
+    class Meta:
+        verbose_name_plural = 'collections'
+
+    def __str__(self):
+        return self.name
+
+
+SIZE_CHOICES = (
+    ('small', 'SMALL'),
+    ('large', 'LARGE')
+)
+
+class Feature(models.Model):
+    collection = models.ForeignKey(Collection, related_name='feature', on_delete=models.CASCADE)
+    scent = models.CharField(max_length =255)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=255, default='small')
+    price = models.FloatField()
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural= 'Features'
+        ordering=('-created', )
+
+    def __str__(self):
+        return self.scent
+
+
+
+
+
+
