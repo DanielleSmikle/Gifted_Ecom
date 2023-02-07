de0215a6dbc4a9065d472dac0aded409c127651a
DanielleSmikle <dsmikle97@gmial.com>
2023-02-06 22:45:43 -0500

    context_processor

--- cart/cart.py
+++ cart/cart.py
@@ -1,5 +1,4 @@
 
-
 class Cart():
     """
     A base Cart class, providing soem default behaviors that can be in herrited or overrided, as necessary.
@@ -7,5 +6,10 @@ class Cart():
     """
 #initialize method, function will be ran right away with new obj created 
     def __init__(self, request):
-       self.session= request.session
-       cart = self.session.get('skey')
+        self.session = request.session
+        cart = self.session.get('skey')
+        if 'skey' not in request.session:    #if session is unavail. it i will create new 
+            cart = self.session['skey'] = {}
+        self.cart = cart
+
+    
