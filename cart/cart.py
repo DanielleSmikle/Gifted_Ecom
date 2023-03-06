1d1d3f37f309a08655db34cef46b12038c350e76
DanielleSmikle <dsmikle97@gmial.com>
2023-03-05 22:26:32 -0500

    refresh_adding

--- cart/cart.py
+++ cart/cart.py
@@ -1,13 +1,8 @@
-from gifted.models import Feature
-from decimal import Decimal 
 
 
 
 class Cart():
-    """
-    A base Cart class, providing soem default behaviors that can be in herrited or overrided, as necessary.
 
-    """
 #initialize method, function will be ran right away with new obj created 
     def __init__(self, request):
         self.session = request.session
@@ -15,33 +10,4 @@ class Cart():
         if 'skey' not in request.session:    #if session is unavail. it i will create new one 
             cart = self.session['skey'] = {}
         self.cart = cart
-
-    def add(self, feature, qty):
-        # adding and updating the users cart session data 
-
-        feature_id= feature.id 
-
-        if feature_id not in self.cart:
-            self.cart[feature_id] = {'price': str(feature.price), 'qty': int(qty)}
-
-        self.session.modified = True
-
-    
-    def __iter__(self):   #collecting featureid in session data to query database and return features
-        feature_ids = self.cart.keys()
-        features = Feature.features.filter(id__in=feature_ids)
-        cart = self.cart.copy()
-
-        for feature in features:
-            cart[str(feature.id)]['feature'] = feature
-
-        for item in cart.values():
-            item['price'] = Decimal(item['price'])
-            item['total_price'] = item['price'] * item['qty']
-            yield item
-
-
-
-    def __len__(self): #getting cart data/ counting the item qty 
-        return sum(item['qty'] for item in self.cart.values())  #how ever much the qty in each session sum the total and get data
 
--- cart/views.py
+++ cart/views.py
@@ -1,22 +1,10 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from django.http import JsonResponse
 
-from gifted.models import Feature
-from .cart import Cart
+
+
 
 def cart_summary(request): 
-    cart= Cart(request)
-    return render(request, 'gifted/cart/summary.html', {'cart': cart})
+    return render(request, 'gifted/cart/summary.html')
 
-def cart_add(request):
-    cart = Cart(request)
-    if request.POST.get('action') == 'post':
-        feature_id = int(request.POST.get('featureid'))
-        feature_qty = int(request.POST.get('featureqty'))
-        feature = get_object_or_404(Feature, id= feature_id)
-        cart.add(feature=feature, qty= feature_qty)
-        cartqty = cart.__len__()
-        response = JsonResponse({'qty': cartqty})
-        return response 
+
 
