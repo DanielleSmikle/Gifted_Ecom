826b8ab48863f39057138d009c3a090d46b9c21f
DanielleSmikle <dsmikle97@gmial.com>
2023-04-26 23:47:58 -0400

    user_creation_error

--- account/urls.py
+++ account/urls.py
@@ -6,8 +6,8 @@ app_name = 'account'
 
 urlpatterns = [
     path('register/', views.account_register, name ='register'),
-    # path('activate/<slug:uidb64>/<slug:token>)/', views.account_activate, name='activate'),
-    # path('dashboard/' , views.dashboard, name='dashboard'),
+    path('activate/<slug:uidb64>/<slug:token>)/', views.account_activate, name='activate'),
+    path('dashboard/' , views.dashboard, name='dashboard'),     #dashboard
 
 
 ]
